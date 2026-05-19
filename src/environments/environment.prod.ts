1fb8d18420a9e349cfe3a2ec2c51aafb6af40042
murilofsilva <murilofsilva11@gmail.com>
2026-05-19 12:40:23 -0400

    fix conf file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.SEU-DOMINIO.com/api'
+  apiUrl: 'http://tcc-murilo-schollabbackend-ygk9up-91bffd-172-237-53-150.sslip.io/api'
 };

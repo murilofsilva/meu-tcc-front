e4adc0edbb27726f97a53ccca54167ea444200d0
murilofsilva <murilofsilva11@gmail.com>
2026-06-09 15:37:43 -0400

    fix: url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://tcc-murilo-schollabbackend-ygk9up-91bffd-172-237-53-150.sslip.io/api'
+  apiUrl: 'https://schoollab-manager-api.duckdns.org/api'
 };
